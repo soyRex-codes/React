fbbfc0caee8757304f3486ed619614e6e203c5a8
Rajkumar Kushwaha <rajkushwaha0213@gmail.com>
2025-01-09 05:48:53 -0600

    Create api_animals?name='cheetah'.js

---
+++ api_animals?name='cheetah'.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+//import './Header.js'; //not part of this file, just for reference how to import other files from the same directory for the current code execution
+
+const apiEndpoint = 'https://api.api-ninjas.com/v1/animals';
+const apiKey = 'Axqs2pGe6cDhGtcP6nrjQw==EaLoQIP8jexunNPF';
+const name = 'cheetah';
+
+function App() {
+  const [data, setData] = useState([]); // State to store data
+  const [loading, setLoading] = useState(true); // State for loading status
+  const [error, setError] = useState(null); // State for error
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${apiEndpoint}?name=${name}`, {
+          headers: { 'X-Api-Key': apiKey },
+        });
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Fetched Data:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default App;
